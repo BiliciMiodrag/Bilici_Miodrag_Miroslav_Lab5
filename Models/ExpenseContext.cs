b5bfb856d613391cb83d7be375dd9b5607651a28
Miodrag Bilici <bilicimio@gmail.com>
2020-11-04 20:10:08 +0200

    Add project files.

---
+++ Models/ExpenseContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace Bilici_Miodrag_Miroslav_Lab5.Models
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options)
+ : base(options)
+        { }
+        public DbSet<Expense> Expense { get; set; }
+        public DbSet<ExpenseDTO> ExpenseDTO { get; set; }
+    }
+}
